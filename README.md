15fd5885351ee639a4ab808ea4703d9b8d667c97
Micael Nussbaumer <micaelnussbaumer@gmail.com>
2021-08-12 13:28:41 +0000

    First Version

--- README.md
+++ README.md
@@ -2,26 +2,12 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.0.
 
+A sample application showcasing how to use [@imageengine/imageengine-angular]() component for leveraging [ImageEngine's CDN](https://https://imageengine.io) for easy image optimization of your project.
+
+Dive deeper by reading the [article explaining the component usage](https://dev.to/mnussbaumer/image-optimization-in-angular)
+
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `ng serve` or `npm run start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, AfterViewInit } from '@angular/core';
+import { IEDirectives, IEFit } from "@imageengine/imageengine-helpers";
+
+const OPTIONS = [
+    [1920, 920],
+    [960, 460],
+    [480, 230],
+    [240, 115]
+];
+
+const FITS: IEFit[] = [
+    "stretch",
+    "box",
+    "letterbox",
+    "cropbox"
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ie-angular-sample';
+
+export class AppComponent implements AfterViewInit {
+    image_path: string = "ie-loader-images/h-lightbox-3.jpeg";
+    sizes: number[][] = [...OPTIONS];
+    size: number = 3;
+    fits: IEFit[] = [...FITS];
+    fit: IEFit = "cropbox";
+    width: string = `${OPTIONS[3][0]}px`;
+    height: string = `${OPTIONS[3][1]}px`;
+    src_url: string | null = "";
+
+    constructor(private change_detector: ChangeDetectorRef) { }
+
+    ngAfterViewInit(): void {
+	this.set_src_url();
+    }
+    
+    change_size(new_val: number): void {
+	this.size = new_val;
+	this.width = `${this.sizes[new_val][0]}px`;
+	this.height = `${this.sizes[new_val][1]}px`;
+	this.change_detector.detectChanges();
+	this.set_src_url();
+    }
+
+    change_fit(new_val: IEFit): void {
+	this.fit = new_val;
+	this.change_detector.detectChanges();
+	this.set_src_url();
+    }
+
+    set_src_url() {
+	let img = document.querySelector(".ngx-ie-image");
+	if (img) {
+	    this.src_url = img.getAttribute("src");
+	    this.change_detector.detectChanges();
+	}
+    }
 }
